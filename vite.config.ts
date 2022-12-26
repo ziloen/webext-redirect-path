59d4649c23c710677baf68ce180bac09eaa25235
N <34268416+ziloen@users.noreply.github.com>
2022-12-26 16:43:02 +0800

    chore: add sass

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,25 @@
 /// <reference types="vitest" />
 
-import { dirname, relative } from 'path'
+import Vue from '@vitejs/plugin-vue'
+import { dirname, relative } from 'node:path'
+import UnoCSS from 'unocss/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import IconsResolver from 'unplugin-icons/resolver'
+import Icons from 'unplugin-icons/vite'
+import Components from 'unplugin-vue-components/vite'
 import type { UserConfig } from 'vite'
 import { defineConfig } from 'vite'
-import Vue from '@vitejs/plugin-vue'
-import Icons from 'unplugin-icons/vite'
-import IconsResolver from 'unplugin-icons/resolver'
-import Components from 'unplugin-vue-components/vite'
-import AutoImport from 'unplugin-auto-import/vite'
-import UnoCSS from 'unocss/vite'
 import { isDev, port, r } from './scripts/utils'
 
 export const sharedConfig: UserConfig = {
   root: r('src'),
   resolve: {
     alias: {
-      '~/': `${r('src')}/`,
-    },
+      '~/': `${r('src')}/`
+    }
   },
   define: {
-    __DEV__: isDev,
+    __DEV__: isDev
   },
   plugins: [
     Vue(),
@@ -29,11 +29,11 @@ export const sharedConfig: UserConfig = {
         'vue',
         {
           'webextension-polyfill': [
-            ['*', 'browser'],
-          ],
-        },
+            ['*', 'browser']
+          ]
+        }
       ],
-      dts: r('src/auto-imports.d.ts'),
+      dts: r('src/auto-imports.d.ts')
     }),
 
     // https://github.com/antfu/unplugin-vue-components
@@ -44,9 +44,9 @@ export const sharedConfig: UserConfig = {
       resolvers: [
         // auto import icons
         IconsResolver({
-          componentPrefix: '',
-        }),
-      ],
+          componentPrefix: ''
+        })
+      ]
     }),
 
     // https://github.com/antfu/unplugin-icons
@@ -62,19 +62,19 @@ export const sharedConfig: UserConfig = {
       apply: 'build',
       transformIndexHtml(html, { path }) {
         return html.replace(/"\/assets\//g, `"${relative(dirname(path), '/assets')}/`)
-      },
-    },
+      }
+    }
   ],
   optimizeDeps: {
     include: [
       'vue',
       '@vueuse/core',
-      'webextension-polyfill',
+      'webextension-polyfill'
     ],
     exclude: [
-      'vue-demi',
-    ],
-  },
+      'vue-demi'
+    ]
+  }
 }
 
 export default defineConfig(({ command }) => ({
@@ -83,8 +83,8 @@ export default defineConfig(({ command }) => ({
   server: {
     port,
     hmr: {
-      host: 'localhost',
-    },
+      host: 'localhost'
+    }
   },
   build: {
     outDir: r('extension/dist'),
@@ -92,18 +92,18 @@ export default defineConfig(({ command }) => ({
     sourcemap: isDev ? 'inline' : false,
     // https://developer.chrome.com/docs/webstore/program_policies/#:~:text=Code%20Readability%20Requirements
     terserOptions: {
-      mangle: false,
+      mangle: false
     },
     rollupOptions: {
       input: {
         background: r('src/background/index.html'),
         options: r('src/options/index.html'),
-        popup: r('src/popup/index.html'),
-      },
-    },
+        popup: r('src/popup/index.html')
+      }
+    }
   },
   test: {
     globals: true,
-    environment: 'jsdom',
-  },
+    environment: 'jsdom'
+  }
 }))
